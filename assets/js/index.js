96ef65495c5e77028b8aed4bf396128bd351d53a
unknown <arifkhan32587@gmail.com>
2025-07-28 11:56:06 +0530

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,9 @@ function Sidebar() {
       },
        slidesPerView: 3,
        loop: true,
-
-
-
+       autoplay: {
+          delay: 3000,
+        },
         breakpoints: {
             30: {
               slidesPerView: 1,
@@ -26,3 +26,13 @@ function Sidebar() {
         }
 });
 
+// const imgClick = document.querySelector(".img-click");
+// const boxImg = document.getElementById("box-img");
+
+// function OpanImg(pic) {
+//     imgClick.style.display="flex";
+//     boxImg.src=pic
+// }
+// function CloseImg() {
+//     imgClick.style.display="none";
+// }
